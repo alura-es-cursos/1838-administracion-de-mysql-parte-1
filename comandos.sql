80db357a42aaa1821890cb28471a546537e93205
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-07-16 14:45:23 -0300

    Comandos Aula 1

---
+++ comandos.sql
@@ -0,0 +1,5 @@
+/* 
+Bienvenido al curso de Administración de MySQL: Seguridad y optimización de la base de datos. 
+
+Te invito a preparar el ambiente para iniciar con nuestro entrenamiento. ¡Éxitos!
+*/
