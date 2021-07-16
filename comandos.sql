d55c44e699e5c444971174f3499bf6b6a3967d40
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-07-16 14:51:18 -0300

    Comandos Aula 2

--- comandos.sql
+++ comandos.sql
@@ -3,3 +3,10 @@ Bienvenido al curso de Administración de MySQL: Seguridad y optimización de la
 
 Te invito a preparar el ambiente para iniciar con nuestro entrenamiento. ¡Éxitos!
 */
+
+
+/* PROYECTO DEL AULA ANTERIOR 1 
+
+Llegó del momento de practicar los comandos que aprenderás durante el desarrollo de esta clase. 
+
+Recuerda que nuestro foro está a disposición en caso de que tengas inquietudes. */
